a29773f0899557da88aa6cace9ae97be46cb57b0
Daniel Moreira <danieldenis01@gmail.com>
2017-01-30 19:36:17 -0200

    Verificação de Status

--- lib/zenvia/request.rb
+++ lib/zenvia/request.rb
@@ -4,20 +4,35 @@ module Zenvia
 
     base_uri 'https://api-rest.zenvia360.com.br/services/'
     SEND_PATH = "/send-sms"
+    GET_STATUS_PATH = "/get-sms-status"
 
-    attr_reader :default_options, :params, :response
+    attr_reader :default_options, :params, :response, :username, :password
 
     def initialize(username, password)
+      @username = username
+      @password = password
       @default_options = {
-        basic_auth: { username: username, password: password },
-        headers: {"Content-Type" => "application/json", "Accept" => "application/json"}
+        headers: {
+          "Content-Type" => "application/json",
+          "Accept" => "application/json",
+          "Authorization" => "Basic #{basic_auth_encoded}"
+        }
       }
     end
 
     def perform(options)
       options.merge!(default_options)
       @response = self.class.post(SEND_PATH, options)
-      self.response.body
+      parse_response
+    end
+
+    def check_status(sms_id)
+      @response = self.class.get("#{GET_STATUS_PATH}/#{sms_id}")
+      parse_response
+    end
+
+    def basic_auth_encoded
+      Base64.encode64("#{username}:#{password}")
     end
 
     private
